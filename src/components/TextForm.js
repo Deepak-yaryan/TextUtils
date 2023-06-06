513ddd609474215bb4a9bc39fc252b55b22a023c
Deepak <deepakyaryan898@gmail.com>
2023-06-06 23:00:46 +0530

    Final Commit

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -40,28 +40,30 @@ export default function TextForm(props) {
 
   const [text, setText] = useState('');
   let minus = text.split(" ").length - 1;
-  let word = text.split(/\s+/).filter((element)=>{return element.length!==0}).length;
+  let word = text.split(/\s+/).filter((element) => { return element.length !== 0 }).length;
   let character = text.length - minus;
   // setText("New Text");
   return (
     <>
-      <div className='container' style={{color: props.mode === 'dark'?'white':'black'}}>
-        <h2>{props.heading}</h2>
-        <div className='mb-3'>
-          <textarea className='form-control' value={text} name="" onChange={handleOnChange} id="my-box" style={{backgroundColor: props.mode === 'dark' ? 'grey' : 'white', color: props.mode === 'dark'?'white':'black'}} rows="8"></textarea>
+      <div style={{minHeight: "100vh"}}>
+        <div className='container' style={{ color: props.mode === 'dark' ? 'white' : 'black'}}>
+          <h2>{props.heading}</h2>
+          <div className='mb-3'>
+            <textarea className='form-control' value={text} name="" onChange={handleOnChange} id="my-box" style={{ backgroundColor: props.mode === 'dark' ? 'grey' : 'white', color: props.mode === 'dark' ? 'white' : 'black' }} rows="8"></textarea>
+          </div>
+          <button disabled={text.length === 0} className="btn btn-primary mx-1 my-2" onClick={handleUpClick}>Convert to Uppercase</button>
+          <button disabled={text.length === 0} className="btn btn-primary mx-1 my-2" onClick={handleLoClick}>Convert to Lowercase</button>
+          <button disabled={text.length === 0} className="btn btn-primary mx-1 my-2" onClick={handleClearClick}>Clear Text</button>
+          <button disabled={text.length === 0} className="btn btn-primary mx-1 my-2" onClick={handleCopy}>Copy Text</button>
+          <button disabled={text.length === 0} className="btn btn-primary mx-1 my-2" onClick={handleExtraSpaces}>Remove Extra Spaces</button>
         </div>
-        <button disabled={text.length===0} className="btn btn-primary mx-1 my-2" onClick={handleUpClick}>Convert to Uppercase</button>
-        <button disabled={text.length===0} className="btn btn-primary mx-1 my-2" onClick={handleLoClick}>Convert to Lowercase</button>
-        <button disabled={text.length===0} className="btn btn-primary mx-1 my-2" onClick={handleClearClick}>Clear Text</button>
-        <button disabled={text.length===0} className="btn btn-primary mx-1 my-2" onClick={handleCopy}>Copy Text</button>
-        <button disabled={text.length===0} className="btn btn-primary mx-1 my-2" onClick={handleExtraSpaces}>Remove Extra Spaces</button>
-      </div>
-      <div className="container my-2" style={{color: props.mode === 'dark'?'white':'black'}}>
-        <h1>Your Text Summary</h1>
-        <p>{word} words and {character} characters</p>
-        <p>{0.008 * word} Minutes to Read</p>
-        <h2>Preview</h2>
-        <div>{text.length>0?<textarea className='form-control' value={text} name="" id="my-box" style={{backgroundColor: props.mode === 'dark' ? 'grey' : 'white', color: props.mode === 'dark'?'white':'black'}} rows="4"></textarea>:"Nothing To Preview"}</div>
+        <div className="container my-2" style={{ color: props.mode === 'dark' ? 'white' : 'black' }}>
+          <h1>Your Text Summary</h1>
+          <p>{word} words and {character} characters</p>
+          <p>{0.008 * word} Minutes to Read</p>
+          <h2>Preview</h2>
+          <div>{text.length > 0 ? <textarea className='form-control' value={text} name="" id="my-box" style={{ backgroundColor: props.mode === 'dark' ? 'grey' : 'white', color: props.mode === 'dark' ? 'white' : 'black' }} rows="4"></textarea> : "Nothing To Preview"}</div>
+        </div>
       </div>
     </>
   )
